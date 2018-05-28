8d7a6fc487466bc67522f9dae8f2df8a84e84b29
Pradolini Fernando <fpradolini@hotmail.com>
2018-05-28 11:40:50 -0300

    Se agrega proyecto symfony 3.4 con urls para demo de themas materialize y bootstrap

---
+++ src/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller {
+
+    /**
+     * Renderiza la vista demo del theme bootstrap del stgtheme bundle
+     * 
+     * @Route("/bootstrap-theme", name="bootstrap-theme")
+     */
+    public function bootstrapThemeAction(Request $request) {
+        return $this->render('@STGTheme/Default/base.html.twig');
+    }
+
+    /**
+     * Renderiza la vista demo del theme materialize del stgtheme bundle
+     * 
+     * @Route("/materialize-theme", name="materialize-theme")
+     */
+    public function materializeThemeAction(Request $request) {
+        return $this->render('@STGTheme/Default/baseMaterializecss.html.twig');
+    }
+
+}
